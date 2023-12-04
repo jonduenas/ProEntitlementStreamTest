09e6afeb41597531b874b0440ecdea741b585ef0
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2023-12-04 14:24:53 -0500

    Initial Commit

---
+++ ProEntitlementStreamTest/ProEntitlementStreamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProEntitlementStreamTestApp.swift
+//  ProEntitlementStreamTest
+//
+//  Created by Jon Duenas on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ProEntitlementStreamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

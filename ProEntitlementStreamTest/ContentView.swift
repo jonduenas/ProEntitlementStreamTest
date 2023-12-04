9e3071d72640cbb11c6c5a99cd292dc989faca03
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2023-12-04 14:36:20 -0500

    Example of issue.

--- ProEntitlementStreamTest/ContentView.swift
+++ ProEntitlementStreamTest/ContentView.swift
@@ -5,9 +5,35 @@
 //  Created by Jon Duenas on 12/4/23.
 //
 
+import Dependencies
+import DependenciesMacros
 import SwiftUI
 
+extension DependencyValues {
+    public var proEntitlementStream: ProEntitlementStream {
+        get { self[ProEntitlementStream.self] }
+        set { self[ProEntitlementStream.self] = newValue }
+    }
+}
+
+@DependencyClient
+public struct ProEntitlementStream {
+    public var entitlement: () -> AsyncStream<Bool> = { .never }
+}
+
+extension ProEntitlementStream: DependencyKey {
+    public static let liveValue = ProEntitlementStream {
+        .finished
+    }
+}
+
+class Model {
+    @Dependency(\.proEntitlementStream) private var proStream
+}
+
 struct ContentView: View {
+    let model: Model
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +46,11 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(model: withDependencies {
+        $0.proEntitlementStream = ProEntitlementStream(entitlement: {
+            AsyncStream<Bool>.never
+        })
+    } operation: {
+        Model()
+    })
 }
